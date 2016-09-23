4dbbf004d1663d2b61104e47be006772b57a030f
Yuan-Hsin Huang <yuanhsin7211@gmail.com>
2016-09-23 00:18:22 -0700

    Update 197. Rising Temperature.sql

--- 197. Rising Temperature.sql
+++ 197. Rising Temperature.sql
@@ -14,4 +14,15 @@
 |  2 |
 |  4 |
 +----+
+--/* Solution 1 */--
+SELECT w1.Id 
+FROM Weather as w1 
+INNER(LEFT) JOIN Weather as w2 
+on  w1.Temperature > w2.Temperature
+WHERE DATEDIFF(w1.Date,w2.Date)=1;
 
+--/* Solution 2 */--
+SELECT w1.Id 
+FROM Weather as w1 
+INNER JOIN Weather as w2 
+ON TO_DAYS(w1.Date) = TO_DAYS(w2.Date) + 1 AND w1.Temperature > w2.Temperature;
